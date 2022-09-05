ea45e4b6ad560997e60f2e25b21d6bdc36866c56
ogola5 <ogolaevance5@gmail.com>
2022-09-05 23:39:51 +0300

    completed

--- test/index.js
+++ test/index.js
@@ -1,27 +1,53 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const hasTargetSum = require("../index");
+function hasTargetSum(array, target) {
+	// Write your algorithm here
+	for(i=0; i < array.length; i++){
+		let comp = array.splice(0, 1); 
+		if(array.includes(target-comp)){
+			return true
+		}
+	}
+	return false
+}
 
-describe("hasTargetSum", () => {
-  it("returns true when the array is [3, 8, 12, 4, 11, 7] and the target is 10", () => {
-    expect(hasTargetSum([3, 8, 12, 4, 11, 7], 10)).to.be.true;
-  });
-  it("returns true when the array is [22, 19, 4, 6, 30] and the target is 25", () => {
-    expect(hasTargetSum([22, 19, 4, 6, 30], 25)).to.be.true;
-  });
-  it("returns false when the array is [1, 2, 5] and the target is 4", () => {
-    expect(hasTargetSum([1, 2, 5], 4)).to.be.false;
-  });
-  it("returns true when the array is [-7, 10, 4, 8] and the target is 3", () => {
-    expect(hasTargetSum([-7, 10, 4, 8], 3)).to.be.true;
-  });
-  it("returns true when the array is [1, 2, 3, 4] and the target is 5", () => {
-    expect(hasTargetSum([1, 2, 3, 4], 5)).to.be.true;
-  });
-  it("returns true when the array is [2, 2, 3, 3] and the target is 4", () => {
-    expect(hasTargetSum([2, 2, 3, 3], 4)).to.be.true;
-  });
-  it("returns false when the array is [4] and the target is 4", () => {
-    expect(hasTargetSum([4], 5)).to.be.false;
-  });
-});
+//------------------------------------------------------------------------------
+// BIG O TIME COMPLEXITY
+//------------------------------------------------------------------------------
+// O(n)
+
+//------------------------------------------------------------------------------
+// PSEUDOCODE
+//------------------------------------------------------------------------------
+// Loop through array:
+//		destructively remove first number in array
+//		subtract from target, check if value exists in array
+//		return true if found in array
+//	return false
+
+//------------------------------------------------------------------------------
+// EXPLANATION OF THE SOLUTION
+//------------------------------------------------------------------------------
+/* function hasTargetSum accepts and array and a target as an argument and iterates over that array using a for loop.
+  The for loop used the splice method to desctructively remove the first number in the array and save it to the comp variable.
+  The loop subtracts the spliced number from the target number to obtain the difference of those two numbers
+  Then using an includes method to determine if the difference of those two numbers exists in the array.
+  If so it returns true, if not it reiterates over the array of remaining numbers until it recieves a true statement in which case it returns true or reaches the end of the loop and returns false.
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+	
+	console.log("");
+	
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+	
+	console.log("");
+	
+	console.log("Expecting: false");
+	console.log("=>", hasTargetSum([1, 2, 5], 4));
+}
+
+module.exports = hasTargetSum;
